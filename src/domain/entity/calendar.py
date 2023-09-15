eeb81e8f502f6ad36bfabb5d068837a775f9f2f3
qodot <waitingforqodot@gmail.com>
2023-09-15 18:58:11 +0900

    연도, 주 모델과 속성을 정의하고 캘린더 모델에 포함

--- src/domain/entity/calendar.py
+++ src/domain/entity/calendar.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import datetime
 import uuid
 
@@ -7,3 +9,14 @@ class Calendar:
     name: str
     birthday: datetime.date
     lifespan: int
+    years: list[Year]
+
+
+class Year:
+    yearnum: int
+    weeks: list[Week]
+
+
+class Week:
+    yearnum: int
+    weeknum: int
