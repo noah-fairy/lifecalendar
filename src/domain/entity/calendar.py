4c647e492d7ad9ce435a956447a1a7a0747edc08
qodot <waitingforqodot@gmail.com>
2023-09-16 15:02:29 +0900

    `Calendar.create` 구현

--- src/domain/entity/calendar.py
+++ src/domain/entity/calendar.py
@@ -11,12 +11,50 @@ class Calendar:
     lifespan: int
     years: list[Year]
 
+    def __init__(
+        self,
+        *,
+        id: uuid.UUID,
+        name: str,
+        birthday: datetime.date,
+        lifespan: int,
+        years: list[Year],
+    ) -> None:
+        self.id = id
+        self.name = name
+        self.birthday = birthday
+        self.lifespan = lifespan
+        self.years = years
+
+    @classmethod
+    def create(cls, name: str, birthday: datetime.date, lifespan: int) -> Calendar:
+        return cls(
+            id=uuid.uuid4(),
+            name=name,
+            birthday=birthday,
+            lifespan=lifespan,
+            years=[
+                Year(yearnum=yearnum)
+                for yearnum in range(birthday.year, birthday.year + lifespan + 1)
+            ],
+        )
+
 
 class Year:
     yearnum: int
     weeks: list[Week]
 
+    def __init__(self, *, yearnum: int) -> None:
+        self.yearnum = yearnum
+        self.weeks = [
+            Week(yearnum=yearnum, weeknum=weeknum) for weeknum in range(1, 53)
+        ]
+
 
 class Week:
     yearnum: int
     weeknum: int
+
+    def __init__(self, *, yearnum: int, weeknum: int) -> None:
+        self.yearnum = yearnum
+        self.weeknum = weeknum
--- test/domain/entity/test_calendar.py
+++ test/domain/entity/test_calendar.py
@@ -1,2 +1,17 @@
+import datetime
+
+from src.domain.entity.calendar import Calendar
+
+
 class TestCalendar:
-    pass
+    def test_create(self):
+        name = "고도"
+        birthday = datetime.date(1988, 6, 21)
+        lifespan = 80
+        calendar = Calendar.create(name, birthday, lifespan)
+
+        assert calendar.name == name
+        assert calendar.birthday == birthday
+        assert calendar.lifespan == lifespan
+        assert len(calendar.years) == lifespan + 1
+        assert len(calendar.years[0].weeks) == 52
