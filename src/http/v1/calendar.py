a9951b8bfb9b8b58c3fd09a142f137d288962aae
qodot <waitingforqodot@gmail.com>
2023-09-22 16:07:08 +0900

    캘린더 수정 기능을 HTTP로 공개

--- src/http/v1/calendar.py
+++ src/http/v1/calendar.py
@@ -1,10 +1,12 @@
 import datetime
+import uuid
 
 from fastapi import APIRouter
 from pydantic import BaseModel
 
 from src.appl.container import container
 from src.appl.create_calendar import CreateCalendar
+from src.appl.update_calendar import UpdateCalendar
 
 api_router_calendar = APIRouter(prefix="/calendar")
 
@@ -18,3 +20,16 @@ class CreateCalendarReq(BaseModel):
 @api_router_calendar.post("/create")
 async def create(req: CreateCalendarReq):
     container.resolve(CreateCalendar).run(req.name, req.birthday, req.lifespan)
+
+
+class UpdateCalendarReq(BaseModel):
+    name: str
+    birthday: datetime.date
+    lifespan: int
+
+
+@api_router_calendar.post("/{calendar_id}/update")
+async def update(calendar_id: uuid.UUID, req: UpdateCalendarReq):
+    container.resolve(UpdateCalendar).run(
+        calendar_id, req.name, req.birthday, req.lifespan
+    )
