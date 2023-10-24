2dfe0ff84e63abe1ec7634098e085ddaad97bc66
qodot <waitingforqodot@gmail.com>
2023-10-24 19:45:08 +0900

    CORS 설정 추가

--- src/http/main.py
+++ src/http/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.appl.container import compose_container
 from src.http.v1.router import api_router_v1
@@ -6,7 +7,15 @@ from src.infra.db.mapper import map_between_model_and_schema
 
 map_between_model_and_schema()
 compose_container()
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://lifecalendar-web.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
