a84c7a5cd5f2c3e3f9b9e81f277ee2ff67b56d57
qodot <waitingforqodot@gmail.com>
2023-10-26 14:32:09 +0900

    user, auth_session 테이블 추가

--- src/infra/db/schema.py
+++ src/infra/db/schema.py
@@ -1,4 +1,5 @@
 import datetime
+
 from sqlalchemy import Column, Date, DateTime, Integer, MetaData, String, Table, Uuid
 
 metadata = MetaData()
@@ -10,5 +11,24 @@ calendar = Table(
     Column("name", String(100)),
     Column("birthday", Date),
     Column("lifespan", Integer),
+    Column("inserted_at", DateTime, default=datetime.datetime.utcnow),
+)
+
+user = Table(
+    "user",
+    metadata,
+    Column("id", Uuid, primary_key=True),
+    Column("email", String(500)),
+    Column("password", String(500)),
+    Column("inserted_at", DateTime, default=datetime.datetime.utcnow),
+)
+
+auth_session = Table(
+    "auth_session",
+    metadata,
+    Column("id", Uuid, primary_key=True),
+    Column("user_id", Uuid),
+    Column("expired_at", DateTime),
+    Column("last_accessed_at", DateTime),
     Column("inserted_at", DateTime, default=datetime.datetime.utcnow),
 )
